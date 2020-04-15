5f38003dfbee05156fd1c62f1fee5873451bc314
Eeshan Aza <37574333+code-eshan@users.noreply.github.com>
2020-04-15 15:06:44 +0530

    sample functional and class components
    created sample functional and class components to make the work easy

---
+++ fstore/src/components/sampleClassComp.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class SampleComponent extends Component {
+    render () {
+        return <h1>Sample Class Component</h1>
+    }
+}
+
+export default SampleComponent;
---
+++ fstore/src/components/sampleFunctionalComp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sample() {
+    return <h1>Sample Functional Component</h1>
+}
+
+export default Sample
