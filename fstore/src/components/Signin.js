7d84a143e388583329c99c10d39802ab7d0678a3
Eeshan Aza <37574333+code-eshan@users.noreply.github.com>
2020-04-16 15:40:51 +0530

    Update SignIn.js
    updated signin

--- fstore/src/components/Signin.js
+++ fstore/src/components/Signin.js
@@ -5,16 +5,16 @@ export class Signin extends Component {
     render() {
         return (
             <div>
-                
-                <form class="form-signin">
-                <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus></input>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
 
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">&copy; 2020-2021</p>
+                <form className="form-signin">
+                    <h1 className="h3 mb-3 font-weight-normal">Sign In</h1>
+                    <label for="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus></input>
+                    <label for="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required></input>
+
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    <p className="mt-5 mb-3 text-muted">&copy; 2020-2021</p>
 
                 </form>
 
