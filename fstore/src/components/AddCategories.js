b3e6e962ba7dba0e15cc6b2667477d4eb8c6f75f
Eeshan Aza <37574333+code-eshan@users.noreply.github.com>
2020-04-17 18:41:29 +0530

    Updated AddCategory component
    created elements for the user to enter category title with two buttons for add and view, in /admin/categories

--- fstore/src/components/AddCategories.js
+++ fstore/src/components/AddCategories.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import AdminNav from './AdminNav'
 import Footer from './Footer'
-import { Switch, Route } from 'react-router-dom'
+import './compCss/signin.css'
 
 class AddCategories extends Component {
     render() {
         return (
             <div>
                 <AdminNav></AdminNav>
-                <h1>Categories section</h1>
+                <h1>Add Product Categories</h1>
+
+                <div>
+                    <label className="lbl">Category Name : </label>
+                    <input type="text" className="inpt" placeholder="Category Name"></input>
+                </div>
+                <button type="submit" className="btn">Add Category</button>
+                <button type="button" className="btn">View Exisiting Categories</button>
+
                 <Footer></Footer>
             </div>
         )
