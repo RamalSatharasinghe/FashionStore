84f2478bc4c0907b24b7e21145d075c036c01c0a
Eeshan Aza <37574333+code-eshan@users.noreply.github.com>
2020-04-19 17:13:51 +0530

    just some changes

--- fstore/src/App.js
+++ fstore/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
 import Admin from './components/Admin';
 import Home from './components/Home';
 
--- fstore/src/components/AddCategories.js
+++ fstore/src/components/AddCategories.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
 import AdminNav from './AdminNav'
 import Footer from './Footer'
-import './compCss/signin.css'
 
-class AddCategories extends Component {
+export class AddCategories extends Component {
     render() {
         return (
             <div>
-                <AdminNav></AdminNav>
-                <h1>Add Product Categories</h1>
-
                 <div>
-                    <label className="lbl">Category Name : </label>
-                    <input type="text" className="inpt" placeholder="Category Name"></input>
-                </div>
-                <button type="submit" className="btn">Add Category</button>
-                <button type="button" className="btn">View Exisiting Categories</button>
+                    <AdminNav></AdminNav>
+                    <h1>Add Product Categories</h1>
+                    <form action={this.handleSubmit} method="post">
+                        <div>
+                            <label className="lbl">Category Name : </label>
+                            <input type="text" className="inpt" placeholder="Category Name"></input>
+                        </div>
+                        <button type="submit" className="btn">Add Category</button>
+                        <button type="button" className="btn">View Exisiting Categories</button>
+                    </form>
 
-                <Footer></Footer>
+
+                    <Footer></Footer>
+                </div>
             </div>
         )
     }
 }
 
 export default AddCategories
+
