4b0b59902a60d5ca6d189d77a081ef13398f0576
code-eshan <labeeshanr@gmail.com>
2020-04-22 17:11:39 +0530

    Update admin_storemanagers.js
    POST Request which adds the name, password and email of the store manager to the mongodb works fine.

--- fstorebackend/routes/admin_storemanagers.js
+++ fstorebackend/routes/admin_storemanagers.js
@@ -12,6 +12,37 @@ let StoreManagers = require('../models/storemanagers');
 //POST ADD STORE MANAGERS
 router.post('/addStoreManagers', function (req,res) {
     console.log('Store Managers Post Called');
+    console.log(req.body);
+
+    let name = req.body.storeManagerName;
+    let email = req.body.storeManagerEmail;
+    let password = req.body.storeManagerPassword;
+
+    let errors = req.validationErrors;
+
+    if(errors) {
+        console.log('errors reported in store managers post');
+    } else {
+        StoreManagers.findOne({sEmail:email}, function (err,storeManager) {
+            if(storeManager) {
+                console.log('Store Manager Already Registered.');
+                res.redirect('/admin/storemanagers');
+            } else {
+                let storemanager = new StoreManagers({
+                    sName : name,
+                    sEmail: email,
+                    sPassword: password
+                });
+
+                storemanager.save(function (err) {
+                    if(err) return console.log(err);
+                    console.log('Store Manager Successfully Added.');
+                    res.redirect('/admin/storemanagers');
+                })
+            }
+        })
+    }
+
 });
 
 //Exports
