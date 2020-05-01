a8bd3e4f398ecb9e144da23a25620581c88e411f
TRupasinghe <52608955+TRupasinghe@users.noreply.github.com>
2020-05-02 02:08:23 +0530

    Input handling in Add Product Component

--- fstore/src/components/AddProducts.js
+++ fstore/src/components/AddProducts.js
@@ -8,32 +8,114 @@ import ProductStockNav from "./ProductStockNav";
 
 class AddProducts extends Component{
 
+
+
+
+
+
+    constructor(props) {
+        super(props);
+        this.onChangeProductName = this.onChangeProductName.bind(this);
+        this.onchangeProductCategory = this.onchangeProductCategory.bind(this);
+        this.onchangeProductPrice = this.onchangeProductPrice.bind(this);
+        this.onchangeProductQuantity = this.onchangeProductQuantity.bind(this);
+        this.onchangeProductDiscount  = this.onchangeProductDiscount.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state ={
+
+            product_name:'',
+            product_category:'',
+            product_price:'',
+            product_quantity:'',
+            product_discount:''
+
+
+        }
+
+
+    }
+            onChangeProductName(e){
+                this.setState({
+                    product_name:e.target.value
+
+                })
+            }
+
+            onchangeProductCategory(e){
+                this.setState({
+                    product_category:e.target.value
+                })
+            }
+
+            onchangeProductPrice(e){
+                this.setState({
+                    product_price:e.target.value
+                })
+            }
+
+            onchangeProductQuantity(e){
+                this.setState({
+                    product_quantity:e.target.value
+                })
+            }
+
+
+            onchangeProductDiscount(e){
+                this.setState({
+                    product_discount:e.target.value
+                })
+            }
+
+
+            onSubmit(e){
+               e.preventDefault();
+
+               this.setState({
+                   product_name:'',
+                   product_category:'',
+                   product_price:'',
+                   product_quantity:'',
+                   product_discount:''
+               })
+
+            }
+
+
+
+
+
     render() {
         return (
             <div>
+
+
                 <ProductStockNav/>
                 <div className="container pt-3 w-75 ">
                 <div className="card">
                     <h4 className="card-header bg-dark text-white">Add New Product</h4>
                     <div className="card-body bg-light">
 
-                        <form>
+                        <form onSubmit={this.onSubmit}>
 
                             <div className="form-group">
                                 <label className="float-left"> Product Name:</label>
-                                <input type="text" className='form-control w-100' />
+                                <input type="text" className='form-control w-100'
+                                       value={this.state.product_name}
+                                       onChange={this.onChangeProductName}/>
 
 
                             </div>
 
                             <div className="form-group">
                                 <label className="float-left"> Category:</label>
-                                <select className="browser-default custom-select  " >
-                                    <option selected>Choose...</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
+                                <select className="browser-default custom-select " >
+
+                                    <option> T-Shirt</option>
+                                    <option> Denium</option>
+
+
+
                                 </select>
 
 
@@ -50,21 +132,27 @@ class AddProducts extends Component{
 
                             <div className="form-group">
                                 <label className="float-left mt-6"> Unit Price:</label>
-                                <input type="text" className='form-control w-100'/>
+                                <input type="text" className='form-control w-100'
+                                       value={this.state.product_price}
+                                       onChange={this.onchangeProductPrice}/>
 
 
                             </div>
 
                             <div className="form-group">
                                 <label className="float-left"> Quantity :</label>
-                                <input type="text" className='form-control w-100'/>
+                                <input type="text" className='form-control w-100'
+                                       value={this.state.product_quantity}
+                                        onChange={this.onchangeProductQuantity}/>
 
 
                             </div>
 
                             <div className="form-group">
                                 <label className="float-left"> Discount :</label>
-                                <input type="text" className='form-control w-100'/>
+                                <input type="text" className='form-control w-100'
+                                       value={this.state.product_discount}
+                                       onChange={this.onchangeProductDiscount}/>
 
 
                             </div>
@@ -99,4 +187,4 @@ class AddProducts extends Component{
 
 }
 
-export default AddProducts
+export default AddProducts;
