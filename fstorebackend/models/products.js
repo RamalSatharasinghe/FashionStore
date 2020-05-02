8ead2cd53d24525dbac7b8a687d4a8c8eb2e242e
TRupasinghe <52608955+TRupasinghe@users.noreply.github.com>
2020-05-02 23:58:59 +0530

    Creating Add Products Schema and some minor changers

---
+++ fstorebackend/models/products.js
@@ -0,0 +1,48 @@
+let mongoose = require('mongoose');
+
+//Product Schema
+let ProductSchema = mongoose.Schema({
+
+            name : {
+                type: String,
+                required: true
+            },
+
+            category:{
+
+                type: String,
+                required: true
+            },
+
+            image:{
+
+                type:String
+            },
+
+            price:{
+
+                type:String,
+                required:true
+            },
+
+            quantity:{
+
+                type:String,
+                required:true
+             },
+
+
+            discount:{
+
+                type:String,
+                required:true
+            }
+
+
+
+
+
+
+});
+
+let Category = module.exports = mongoose.model('Product',ProductSchema);
