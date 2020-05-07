577173772cca407bc0c2b70fc36110a798b368d5
Ramal Satharasinghe <ramallakshan@gmail.com>
2020-05-07 23:56:29 +0530

    Cart add to the nav bar

--- fstore/src/components/Home.js
+++ fstore/src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Nav from './Nav'
 import Footer from './Footer'
+import HomeComponent from  './home.component'
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <Nav></Nav>
+                <HomeComponent></HomeComponent>
                 <Footer></Footer>
             </div>
         )
--- fstore/src/components/Nav.js
+++ fstore/src/components/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Signin from './Signin';
+import styled from 'styled-components';
+
+import Signin from './signin.component';
+import Signup from'./signup.component';
+import Home from './home.component';
+import {ButtonContainer} from './Button'
+
 
 function Nav() {
     return (
@@ -10,14 +16,15 @@ function Nav() {
 
                 <div >
 
+
                     <nav className="navbar navbar-expand-lg  navbar-dark bg-primary">
-                        <Link to={''} className="navbar-brand">FashionHub</Link>
+                        <Link to={'/home'} className="navbar-brand">FashionHub</Link>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
 
                             <ul className="navbar-nav mr-auto">
                                 <li className="nav-item">
-                                    <Link to={''} className="nav-link">Home</Link>
+                                    <Link to={'/home'} className="nav-link">Home</Link>
 
                                 </li>
                                 <li className="nav-item">
@@ -33,41 +40,54 @@ function Nav() {
 
                             <span className="navbar-text"> <ul className="navbar-nav mr-auto">
                                 <li className="nav-item">
-                                    <Link to={'p'} className="nav-link">SignUp</Link>
+                                    <Link to={'/signup'} className="nav-link">SignUp</Link>
 
                                 </li>
 
                                 <li className="nav-item">
-                                    <Link to={{ pathname: '/signin' }} className="nav-link">Signin</Link>
+                                    <Link to={'/signin'} className="nav-link">Signin</Link>
 
                                 </li>
                             </ul>
-                            </span>
+    </span>
                         </div></nav>
 
-                    {/* <nav className="navbar navbar-light bg-light justify-content-between">
-        <a href={""} className="navbar-brand">Men</a>
-        <a href={""} className="navbar-brand">Women</a>
-        <a href={""}  className="navbar-brand">Kids & Baby</a>
-        <a href={""}  className="navbar-brand">Sportswear</a>
+      <nav className="navbar navbar-light bg-light justify-content-between">
+                        <a href={""} className="navbar-brand">Men</a>
+                        <a href={""} className="navbar-brand">Women</a>
+                        <a href={""}  className="navbar-brand">Kids & Baby</a>
+                        <a href={""}  className="navbar-brand">Sportswear</a>
 
 
-        <form className="form-inline">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                   aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search
-                </button>
-        </form>
-    </nav> */}
-                    <br />
+                        <form className="form-inline">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search"
+                                   aria-label="Search"/>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search
+                            </button>
+                        </form>
+                    <Link to={'/signin'}>
+                        <ButtonContainer>
+                            <span className="mr-2">
+
+                                 <i className="fas fa-cart-arrow-down"/>
+                            </span>
+
+                             My Cart
+                        </ButtonContainer>
+                    </Link>
+                    </nav>
                 </div>
             </div>
 
             <Switch>
-                <Route path="/signin" component={Signin}></Route>
+                <Route exact path = '/signin' component = {Signin}/>
+                <Route exact path = '/signup' component = {Signup}/>
+                <Route exact path = '/home' component = {Home}/>
+
             </Switch>
         </Router>
     )
 }
+
 
 export default Nav
