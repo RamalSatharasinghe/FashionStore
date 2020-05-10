1597c9243b3d3de8f1a3766c4d43c99c73a2cf55
Ramal Satharasinghe <ramallakshan@gmail.com>
2020-05-10 23:29:51 +0530

    Men products list

--- fstore/src/App.js
+++ fstore/src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Admin from './components/Admin';
+
 import Home from './components/Home';
+
+import Product from './components/Product';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="App">
-                    <Switch>
-                        <Route path="/" exact component={Home}></Route>
-                        <Route path="/admin" component={Admin}></Route>
-                    </Switch>
+                    {/*<Switch>*/}
+                    {/*    <Route exact path="/"  component={Home}></Route>*/}
+
+                    {/*</Switch>*/}
+                    <Home/>
+
+
                 </div>
             </Router>
         );
--- fstore/src/components/Button.js
+++ fstore/src/components/Button.js
@@ -11,3 +11,18 @@ padding:0.2rem 0.5rem;
 cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
 `;
+
+export const CartContainer = styled.button`
+
+
+bottom:0;
+right:0;
+padding:0.2rem 0.4rem;
+
+background: var(--lightBlue);
+border:none;
+color:var(--mainWhite);
+font-size:1.4rem;
+border_radius:0.5 0 0 0;
+border-radius:0.5rem 0.5rem 0.5rem 0.5rem;
+`;
